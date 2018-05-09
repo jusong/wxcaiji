0e54d8443a7034575c7b83e04d1f5627f746c5fa
jusong <hanxiaomeng111@gmail.com>
2018-05-09 15:09:10 +0800

    修改文章的版权信息获取方式从列表页获取改为从内容页获取

--- wxcaiji.js
+++ wxcaiji.js
@@ -33,27 +33,29 @@ function HttpPost(data,path,callback = null) {//发送data到path
 
 module.exports = {
     *beforeSendResponse(requestDetail, responseDetail) {
-        if (/mp\/profile_ext\?action=home/i.test(requestDetail.url))
-        { //公众号历史消息页面
+        if (/mp\/profile_ext\?action=home/i.test(requestDetail.url)) //公众号历史消息页面
+        {
             try {
+                var bodyStr = responseDetail.response.body.toString();
+
                 var reg = /var msgList = \'(.*?)\';/;//历史消息
-                var ret = reg.exec(responseDetail.response.body.toString());
+                var ret = reg.exec(bodyStr);
                 var msgList = ret[1];
 
                 reg = /var username = \"\" \|\| \"(.*?)\";/;
-                ret = reg.exec(responseDetail.response.body.toString());
+                ret = reg.exec(bodyStr);
                 var username = ret[1];
 
                 reg = /var headimg = \"(.*?)\"/;
-                ret = reg.exec(responseDetail.response.body.toString());
+                ret = reg.exec(bodyStr);
                 var headimg = ret[1];
 
                 reg = /var nickname = \"(.*?)\" \|\| \"\";/;
-                ret = reg.exec(responseDetail.response.body.toString());
+                ret = reg.exec(bodyStr);
                 var nickname = ret[1];
 
                 reg = /appid: \"(.*?)\",/;
-                ret = reg.exec(responseDetail.response.body.toString());
+                ret = reg.exec(bodyStr);
                 var appid = ret[1];
 
                 HttpPost({msg_list:msgList, user_name:username, head_img:headimg, nick_name:nickname, appid:appid, url:requestDetail.url},"/wxCaiji/saveMsgList");//将匹配到的历史消息json发送到服务器
@@ -62,8 +64,8 @@ module.exports = {
                 console.log(e);
             }
         }
-        else if(/mp\/profile_ext\?action=getmsg/i.test(requestDetail.url))
-        { //历史消息页向下翻页后的json接口
+        else if(/mp\/profile_ext\?action=getmsg/i.test(requestDetail.url)) //历史消息页向下翻页后的json接口
+        {
 
             try {
                 var json = JSON.parse(responseDetail.response.body.toString());
@@ -74,8 +76,45 @@ module.exports = {
                 console.log(e);
             }
         }
-        else if(/mp\/getappmsgext/i.test(requestDetail.url))
-        { //公众号文章阅读量和点赞量接口
+        else if(/\/s\?__biz/i.test(requestDetail.url) || /mp\/rumor/i.test(requestDetail.url) || /appmsg\/show\?__biz/i.test(requestDetail.url))  //公众号文章接口（rumor这个地址是公众号文章被辟谣了）
+        {
+
+            try {
+
+                var bodyStr = responseDetail.response.body.toString();
+
+                var cheerio = require('cheerio');
+                var $ = cheerio.load(bodyStr);
+                var content = $('#js_content').html();
+                var original_url = $('#js_share_source').attr('href');
+
+                var reg = /var _copyright_stat = \"(.*?)\";/;//历史消息
+                var ret = reg.exec(bodyStr);
+                var copyright_stat = ret[1];
+
+                //var fs = require('fs');
+                //fs.writeFile('/tmp/weixin_content', content, function(err) {});
+
+                return new Promise((resolve, reject) => {
+                    var newResponse = responseDetail.response;
+                    HttpPost({content:content, original_url:original_url, copyright_stat:copyright_stat, url:requestDetail.url},"/wxCaiji/saveMsgContent", function (body) {
+                        if (body) {
+                            var reg = /<script nonce="(.*)"/;
+                            var ret = reg.exec(bodyStr);
+                            var nonce = ret[1];
+                            newResponse.body += '<script nonce="'+nonce+'" type="text/javascript">'+body+'</script>';
+                            resolve({response: newResponse});
+                        } else {
+                            resolve(null);
+                        }
+                    });//将文章内容发送到服务器，并获得下一步跳转链接添加到文章结尾
+                });
+            }catch(e){
+                console.log(e);
+            }
+        }
+        else if(/mp\/getappmsgext/i.test(requestDetail.url)) //公众号文章阅读量和点赞量接口
+        {
 
             try {
 
@@ -94,35 +133,6 @@ module.exports = {
 
                 console.log('biz: '+biz+'; mid: '+mid+'; idx: '+idx);
                 HttpPost({biz:biz, mid:mid, idx:idx, msg_ext:responseDetail.response.body.toString()},"/wxCaiji/saveMsgExt");//将文章阅读量点赞量的json发送到服务器
-            }catch(e){
-                console.log(e);
-            }
-        }
-        else if(/s\?__biz/i.test(requestDetail.url) || /mp\/rumor/i.test(requestDetail.url) || /appmsg\/show\?__biz/i.test(requestDetail.url))
-        { //公众号文章接口（rumor这个地址是公众号文章被辟谣了）
-
-            try {
-
-                var cheerio = require('cheerio');
-                var response = responseDetail.response.body.toString();
-                var $ = cheerio.load(response);
-                var content = $('#js_content').html();
-                //var fs = require('fs');
-                //fs.writeFile('/tmp/weixin_content', content, function(err) {});
-                return new Promise((resolve, reject) => {
-                    var newResponse = responseDetail.response;
-                    HttpPost({content:content, url:requestDetail.url},"/wxCaiji/saveMsgContent", function (body) {
-                        if (body) {
-                            var reg = /<script nonce="(.*)"/;
-                            var ret = reg.exec(response);
-                            var nonce = ret[1];
-                            newResponse.body += '<script nonce="'+nonce+'" type="text/javascript">'+body+'</script>';
-                            resolve({response: newResponse});
-                        } else {
-                            resolve(null);
-                        }
-                    });//将文章内容发送到服务器，并获得下一步跳转链接添加到文章结尾
-                });
             }catch(e){
                 console.log(e);
             }
