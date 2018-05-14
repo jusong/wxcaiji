e20e8e40a76db48f32b01d262d91b81197edeb2f
blacknc <blacknc@163.com>
2018-05-15 01:49:21 +0800

    修复参数漏传的bug

--- wxcaiji.js
+++ wxcaiji.js
@@ -1,5 +1,6 @@
-function HttpPost(data,path,callback = null) {//发送data到path
+function HttpPost(data,path,callback) {//发送data到path
     var http = require('http');
+    callback = callback || null;
     content = require('querystring').stringify(data);
     var options = {
         method: "POST",
@@ -32,10 +33,12 @@ function HttpPost(data,path,callback = null) {//发送data到path
 }
 
 module.exports = {
-    *beforeSendResponse(requestDetail, responseDetail) {
-        if (/mp\/profile_ext\?action=home/i.test(requestDetail.url))
-        { //公众号历史消息页面
-            try {
+    *beforeSendResponse(requestDetail, responseDetail)
+    {
+        if (/mp\/profile_ext\?action=home/i.test(requestDetail.url))    //公众号历史消息页面
+        {
+            try
+            {
                 var reg = /var msgList = \'(.*?)\';/;//历史消息
                 var ret = reg.exec(responseDetail.response.body.toString());
                 var msgList = ret[1];
@@ -58,31 +61,33 @@ module.exports = {
 
                 HttpPost({msg_list:msgList, user_name:username, head_img:headimg, nick_name:nickname, appid:appid, url:requestDetail.url},"/wxCaiji/saveMsgList");//将匹配到的历史消息json发送到服务器
 
-            }catch(e){
+            }
+            catch(e)
+            {
                 console.log(e);
             }
         }
-        else if(/mp\/profile_ext\?action=getmsg/i.test(requestDetail.url))
-        { //历史消息页向下翻页后的json接口
+        else if(/mp\/profile_ext\?action=getmsg/i.test(requestDetail.url))  //历史消息页向下翻页后的json接口
+        {
 
-            try {
+            try
+            {
                 var json = JSON.parse(responseDetail.response.body.toString());
-                if (json.general_msg_list != []) {
+                if (json.general_msg_list != [])
+                {
                     HttpPost({msg_list:json.general_msg_list, is_json:1, url:requestDetail.url},"/wxCaiji/saveMsgList");//将历史消息的json发送到服务器
                 }
-            }catch(e){
+            }
+            catch(e)
+            {
                 console.log(e);
             }
         }
-        else if(/mp\/getappmsgext/i.test(requestDetail.url))
-        { //公众号文章阅读量和点赞量接口
-
-            try {
-
+        else if(/mp\/getappmsgext/i.test(requestDetail.url))    //公众号文章阅读量和点赞量接口
+        {
+            try
+            {
                 var body = requestDetail.requestData.toString();
-                var reg = /(^|&)__biz=([^&]*)($|&)/;
-                var ret = reg.exec(body);
-                var biz = ret[2];
 
                 reg = /(^|&)mid=([^&]*)($|&)/;
                 ret = reg.exec(body);
@@ -92,40 +97,80 @@ module.exports = {
                 ret = reg.exec(body);
                 var idx = ret[2];
 
-                console.log('biz: '+biz+'; mid: '+mid+'; idx: '+idx);
-                HttpPost({biz:biz, mid:mid, idx:idx, msg_ext:responseDetail.response.body.toString()},"/wxCaiji/saveMsgExt");//将文章阅读量点赞量的json发送到服务器
-            }catch(e){
+                HttpPost({mid:mid, idx:idx, msg_ext:responseDetail.response.body.toString()},"/wxCaiji/saveMsgExt");//将文章阅读量点赞量的json发送到服务器
+            }
+            catch(e)
+            {
                 console.log(e);
             }
         }
-        else if(/s\?__biz/i.test(requestDetail.url) || /mp\/rumor/i.test(requestDetail.url) || /appmsg\/show\?__biz/i.test(requestDetail.url))
-        { //公众号文章接口（rumor这个地址是公众号文章被辟谣了）
+        else if(/s\?__biz/i.test(requestDetail.url) || /mp\/rumor/i.test(requestDetail.url) || /appmsg\/show\?__biz/i.test(requestDetail.url))  //公众号文章接口（rumor这个地址是公众号文章被辟谣了）
+        {
 
-            try {
-
+            console.log(requestDetail.url);
+            try
+            {
                 var cheerio = require('cheerio');
                 var response = responseDetail.response.body.toString();
                 var $ = cheerio.load(response);
                 var content = $('#js_content').html();
+                var originalUrl = $('#js_share_source').attr('href');
+
+                reg = /var _copyright_stat = \"(.*?)\";/;
+                ret = reg.exec(response);
+                var copyrightStat = ret[1];
+
                 //var fs = require('fs');
                 //fs.writeFile('/tmp/weixin_content', content, function(err) {});
                 return new Promise((resolve, reject) => {
                     var newResponse = responseDetail.response;
-                    HttpPost({content:content, url:requestDetail.url},"/wxCaiji/saveMsgContent", function (body) {
-                        if (body) {
+                    HttpPost({content:content, url:requestDetail.url, copyright_stat:copyrightStat, original_url:originalUrl},"/wxCaiji/saveMsgContent", function (body) {
+                        if (body)
+                        {
                             var reg = /<script nonce="(.*)"/;
                             var ret = reg.exec(response);
                             var nonce = ret[1];
                             newResponse.body += '<script nonce="'+nonce+'" type="text/javascript">'+body+'</script>';
                             resolve({response: newResponse});
-                        } else {
+                        }
+                        else
+                        {
                             resolve(null);
                         }
                     });//将文章内容发送到服务器，并获得下一步跳转链接添加到文章结尾
                 });
-            }catch(e){
+            }
+            catch(e)
+            {
                 console.log(e);
             }
+        } 
+    },
+    *beforeSendRequest(requestDetail)
+    {
+        if(
+                /mmbiz\.qpic\.cn\/mmbiz_jpg\//i.test(requestDetail.url) ||
+                /oth\.eve\.mdt\.qq\.com:8080\/analytics\/upload/i.test(requestDetail.url) ||
+                /badjs\.weixinbridge\.com\/frontend\/reportspeed/i.test(requestDetail.url) ||
+                /mp\.weixin\.qq\.com\/mp\/profile_ext\?action=urlcheck/i.test(requestDetail.url) ||
+                /open\.weixin\.qq\.com\/sdk\/report/i.test(requestDetail.url)
+          )  //禁止访问的接口
+        {
+            console.log('mached url: '+requestDetail.url);
+//            return {
+//                response: {
+//                    statusCode: 404,
+//                    header: { 'content-type': 'text/html' }
+//                }
+//            };
+            //return null;
+            return {
+                response: {
+                    statusCode: 200,
+                    header: { 'content-type': 'text/html' },
+                    body: 'this could be a <string> or <buffer>'
+                }
+            };
         }
     }
 };
